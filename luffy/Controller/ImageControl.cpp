259f78d1e697d018ff6254f07337f76ffc744809
João Gabriel Segato Kruse <j218615@dac.unicamp.br>
2020-10-14 00:35:32 -0300

    Resolvi seg fault (eu acho) | nada aparece na tela

--- luffy/Controller/ImageControl.cpp
+++ luffy/Controller/ImageControl.cpp
@@ -1,6 +1,8 @@
 #include "ImageControl.hpp"
 
-ImageControl::ImageControl(std::shared_ptr<Images> img): img(this->img) {};
+ImageControl::ImageControl(std::shared_ptr<Images> img){
+	this->img = img;
+};
 
 void ImageControl::change_position(int x, int y){
 	this->img->target.x = x;
